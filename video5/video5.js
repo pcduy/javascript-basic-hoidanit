d1d0f8e95784fa2fc128274f4ae9721b684b5b3e
duypham <pcduy142@gmail.com>
2024-07-27 17:52:11 +0700

    chapter2, video 6

--- video5/video5.js
+++
@@ -1,2 +0,0 @@
-console.log('Hello world with HTML');
-let a = '1$2$3$4$5'
---
+++ video6/video6.js
@@ -0,0 +1,10 @@
+console.log('Hello world with HTML'.toUpperCase());
+let a = 5;
+let b = 6;
+let c = 6.9;
+let d = '5';
+let e = Number(d);
+let f = +d;
+console.log('e value: ', e, 'type', typeof e);
+console.log('f value: ', f, 'type', typeof f);
+console.log(a === e, 'a: ', typeof a, 'd: ', typeof d);
