85f46a28f64f23b070a6bb7981970f5b470a5544
duypham <pcduy142@gmail.com>
2024-07-27 22:36:16 +0700

    chapter 2, video 10

---
+++ video10/video10.js
@@ -0,0 +1,17 @@
+console.log('hello world with html');
+let a = 5;
+let b = '5';
+
+console.log('compare a == b: ', 'a = ', a, ' b = ', b, 'result: ', a == b);
+console.log('compare a === b: ', 'a = ', a, ' b = ', b, 'result: ', a === b);
+// === type, value    convention
+// == value
+
+// emty, null, undefined
+
+let c = null;
+console.log('check value: ', c);
+
+let d = '';
+d = 5;
+d = ['11']
