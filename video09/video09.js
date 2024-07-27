82088c9c402b01a93bfa03afe693621bd2bd4cb9
duypham <pcduy142@gmail.com>
2024-07-27 21:51:20 +0700

    chater 2, video 09

---
+++ video09/video09.js
@@ -0,0 +1,22 @@
+console.log('hello world with html');
+
+//key: value
+let a = {
+    name: 'eric',
+    age: '25'
+};
+
+console.log('check data: ', a);
+console.log(`my name's: `, a.name);
+
+//arr = [item1, item2, item3]
+let b = ['mu', 'chelsea', 'liver']
+console.log('type of b: ', typeof b, b);
+console.log('first item: ', b[0], 'second item: ', b[1], 'third intem: ', b[2]);
+
+let family = [
+    { name: 'eric', age: '25' },
+    { name: 'minica', age: '24' }
+]
+
+console.log('my family: ', family)
