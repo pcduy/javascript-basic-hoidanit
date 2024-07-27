1b8cb857cb5ccae4d0d41a5d528b2cdacfc09b8e
duypham <pcduy142@gmail.com>
2024-07-27 23:04:57 +0700

    chapter 3, video 1

---
+++ video11/video11.js
@@ -0,0 +1,10 @@
+console.log('hello world with html');
+
+let arr = ['mu', 'mancity', 'liver', 'chelsea'];
+
+let i = 0;
+for (let i = 0; i < arr.length; i++) {
+    console.log('check variable i: ', arr[i]);
+}
+
+console.log('check value i: ', i);
