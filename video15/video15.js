eea5dafbb0f986f6a2d523bc33c56b016d86fd18
duypham <pcduy142@gmail.com>
2024-07-28 22:01:14 +0700

    chapter 4, video 16

--- video15/video15.js
+++
@@ -1,7 +0,0 @@
-console.log('hello world with html');
-//global
-//block
-
-for (let i = 0; i < 10; i++) {
-    console.log('>>> check value i: ', i);
-}
---
+++ video16/video16.js
@@ -0,0 +1,10 @@
+console.log('hello world with html');
+
+//input parameter
+function sum(a, b) {
+    return a + b;
+}
+
+let c = sum(10, 20);
+
+console.log('sum c = ', c);
