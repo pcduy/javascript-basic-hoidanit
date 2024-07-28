e4db8d11463b90f019cada1e06f356e8d0aa527c
duypham <pcduy142@gmail.com>
2024-07-28 22:37:26 +0700

    video18

---
+++ video18/video18.js
@@ -0,0 +1,12 @@
+console.log('hello world with html');
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+//arrow function
+let sum = (a, b) => {
+    return a + b;
+}
+
+console.log('check sum: ', sum(6, 9));
